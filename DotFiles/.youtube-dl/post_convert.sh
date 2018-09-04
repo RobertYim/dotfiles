4fea5821c880fbec4c22549bc178ecdccd40c99b
Robert Yim <nikolai.jon@gmail.com>
2018-09-04 14:37:04 +0800

    Update macOSSetup, Cleanup Rime, Adjust Folder Structure

---
+++ DotFiles/.youtube-dl/post_convert.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# Convert flv to mkv
+# filename=$( basename "$1" )
+# extension="${filename##*.}"
+# filename="${filename%.*}"
+# dirname=$( dirname "$1" )
+
+# result=$1
+# if [[ "$extension" == "flv" ]]; then
+	# out="$dirname/$filename.mkv"
+	# ffmpeg -i "$1" -c copy "$out"
+	# if [[ $? == 0 ]]; then
+		# rm "$1"
+		# result=$out
+	# fi
+# fi
+# tag -a "未完" "$result"
